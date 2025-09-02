3bd730896d4c3a619b4ef322b3ce2c57ced1e731
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-09-02 10:25:54 -0300

    setting up openapi

--- backend/internal/handler/handlers.go
+++ backend/internal/handler/handlers.go
@@ -6,7 +6,8 @@ import (
 )
 
 type Handlers struct {
-	Health *HealthHandler
+	Health  *HealthHandler
+	OpenAPI *OpenAPIHandler
 }
 
 func NewHandlers(s *server.Server, services service.Services) *Handlers {
